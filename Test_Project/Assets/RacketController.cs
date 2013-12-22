f9e1694c4e8d5d846c55ec7d765009ec86e67cd2
Makoto Taniguchi <world.for.ash@gmail.com>
2013-12-22 14:40:06 +0900

    新規追加

---
+++ Test_Project/Assets/RacketController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RacketController : MonoBehaviour {
+
+	private int Accel = 0;
+
+	RacketController(){
+		Accel = 1000;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody.AddForce (
+			transform.right * Input.GetAxisRaw ("Horizontal") * Accel,
+			ForceMode.Impulse);
+	}
+}
---
+++ Test_Project/Assets/SphereStart.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereStart : MonoBehaviour {
+
+	private int speed = 0;
+
+	SphereStart()
+	{
+		speed = 5;
+	}
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddForce((transform.forward + transform.right) * speed
+		                   , ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
